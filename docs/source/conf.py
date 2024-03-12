1f26e7454d2bfa2fa608c5dc4cae321aff7c23b1
Aneesh Naik <48957123+aneeshnaik@users.noreply.github.com>
2024-03-12 16:05:25 +0000

    Switch docs to book theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,5 +34,5 @@ napoleon_use_ivar = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
