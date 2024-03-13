c32971af4420192a12b388666da0fc33f1e7314e
Aneesh Naik <48957123+aneeshnaik@users.noreply.github.com>
2024-03-13 11:47:32 +0000

    Switch napoleon to numpydoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'lintsampler'
 copyright = '2024, Aneesh Naik'
 author = 'Aneesh Naik'
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'numpydoc']
 templates_path = ['_templates']
 exclude_patterns = []
 html_theme = 'sphinx_book_theme'
