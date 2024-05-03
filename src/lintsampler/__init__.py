3eff7982963483908338cb08c7e1b4fd12e3c61d
Aneesh Naik <48957123+aneeshnaik@users.noreply.github.com>
2024-05-03 09:00:54 +0100

    Add __version__ to __init__ and update python dep in pyproject.toml

--- src/lintsampler/__init__.py
+++ src/lintsampler/__init__.py
@@ -1,4 +1,10 @@
 from .gridsample import gridsample
 from .sample import sample
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("lintsampler")
+except PackageNotFoundError:
+    __version__ = "unknown version"
 
 __all__ = ["gridsample", "sample"]
