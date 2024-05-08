7679b35c4a849f3ff0a68623c594f59cbb8ee657
michael-petersen <petersen.michael.s@gmail.com>
2024-05-08 11:27:08 +0100

    change call structure; add docstrings

--- src/lintsampler/lintsampler.py
+++ src/lintsampler/lintsampler.py
@@ -4,7 +4,7 @@ from .gridsample import _gridsample
 from .freesample import _freesample
 
 class LintSampler:
-    """Draw sample(s) from density function defined for a list of cells (that may or may not be in a grid)
+    """Draw sample(s) from density function defined for a list of cells (that may or may not be in a single grid)
 
     Examples
     --------
@@ -16,19 +16,19 @@ class LintSampler:
     1. A single sample from a 1D grid. The grid spans x=0 to x=10, and has 32
     cells (so 33 edges). 
     
-    >>> grid = np.linspace(0, 10, 33)
+    >>> cells = np.linspace(0, 10, 33)
     >>> def pdfrandom(X): return np.random.uniform(size=X.shape[0])
-    >>> LintSampler(pdfrandom,grid).sample()
-    0.7355598727871656
+    >>> LintSampler(pdfrandom,cells).sample()
+    6.984134639227398
 
     This returns a single scalar: the sampling point within the grid.
     
     2. Multiple samples from a 1D grid (same grid as previous example).
     
-    >>> grid = np.linspace(0, 10, 33)
+    >>> cells = np.linspace(0, 10, 33)
     >>> def pdfrandom(X): return np.random.uniform(size=X.shape[0])
-    >>> LintSampler(pdfrandom,grid).sample(4)
-    array([0.7432799 , 6.64118763, 9.65968316, 5.39087554])
+    >>> LintSampler(pdfrandom,cells).sample(4)
+    array([8.16447008, 5.30536088, 8.96135879, 7.73572977])
 
     This returns a 1D array: the ``N_samples`` sampling points within the grid.
 
@@ -38,11 +38,9 @@ class LintSampler:
     
     >>> x = np.linspace(0, 10, 33)
     >>> y = np.linspace(100, 200, 65)
-    >>> def pdfrandom(X): return np.random.uniform(size=(X.shape[0],X.shape[1]))
-    >>> LintSampler(pdfrandom,grid).sample()
-
-    >>> f = np.random.uniform(size=(33, 65))
-    >>> gridsample(x, y, f=f)
+    >>> cells = (x,y)
+    >>> def pdfrandom(X): return np.random.uniform(size=X.shape[0])
+    >>> LintSampler(pdfrandom,cells).sample()
     array([  7.67294632, 190.45302915])
 
     This returns a 1D array: the single k-D sampling point within the grid.
@@ -51,8 +49,9 @@ class LintSampler:
     
     >>> x = np.linspace(0, 10, 33)
     >>> y = np.linspace(100, 200, 65)
-    >>> f = np.random.uniform(size=(33, 65))
-    >>> gridsample(x, y, f=f, N_samples=5)
+    >>> cells = (x,y)
+    >>> def pdfrandom(X): return np.random.uniform(size=X.shape[0])
+    >>> LintSampler(pdfrandom,cells).sample(5)
     array([[1.35963966e-01, 1.38182930e+02],
            [6.52704300e+00, 1.63109912e+02],
            [4.35226761e+00, 1.49753235e+02],
@@ -71,10 +70,16 @@ class LintSampler:
         pdf : function
             pdf needs to take an input with dimension [...,k] where k is the number of dimensions
 
-        cells : a tuple with the following optional inputs:
-            1. A tuple with k (min,max,num) entries
-            2. A single nd-array that defines the cells
-            3. A tuple of nd-arrays
+        cells : tuple of arrays or list of tuples of arrays
+            If a tuple of arrays, the boundary values for a grid with dimensionality of the length
+            of the tuple. If a list of tuples of arrays, the boundary values for an arbitrary
+            number of grids with dimensionality the length of the tuples.
+
+        rngseed : {None, int, ``numpy.random.Generator``}, optional
+            Seed for ``numpy`` random generator. Can be random generator itself, in
+            which case it is left unchanged. Default is None, in which case new
+            default generator is created. See ``numpy`` random generator docs for
+            more information.
 
         Returns
         -------
@@ -88,21 +93,17 @@ class LintSampler:
         # set the random seed
         self.rng = np.random.default_rng(rngseed)
 
-        # check the validity of the cells
-        if len(cells)==0:
-            raise ValueError("LintSampler.__init__: you must specify an evaluation domain with a tuple of boundaries, arrays, or grid points. See documentation for details.")
-            # or, we could here drop into an adaptive grid selection
-            # or, we could sample on a unit hypercube with dimensions of the pdf
-
-        # check element grid for which sampling method we will use and design the grid
+        # set up the sampling grid under the hood
         self._setgrid(cells)
 
 
 
     def sample(self, N_samples=None, funcargs=()):
-        """Draw samples from the pdf on the grid.
+        """Draw samples from the pdf on the constructed grid(s).
         
-        This function then draws a sample (or N samples) from the specified grid. It first chooses a cell (or N cells with replacement), weighting them by their mass (estimated by the trapezoid rule) then samples from k-linear interpolant within the chosen cell(s).
+        This function then draws a sample (or N samples) from the specified grid. It first chooses a cell 
+        (or N cells with replacement), weighting them by their mass (estimated by the trapezoid rule) 
+        then samples from k-linear interpolant within the chosen cell(s).
         
         Parameters
         ----------
@@ -111,11 +112,8 @@ class LintSampler:
         N_samples : {None, int}, optional
             Number of samples to draw. Default is None, in which case a single
             sample is drawn.
-        seed : {None, int, ``numpy.random.Generator``}, optional
-            Seed for ``numpy`` random generator. Can be random generator itself, in
-            which case it is left unchanged. Default is None, in which case new
-            default generator is created. See ``numpy`` random generator docs for
-            more information.
+        funcargs : optional
+            A tuple of optional arguments to be passed directly to the pdf function call.
 
         Returns
         -------
@@ -223,49 +221,97 @@ class LintSampler:
         pass
 
 
-    def resetgrid(self,grid=()):
+    def resetgrid(self,cells=()):
         """Reset the sample grid without changing the pdf.
         
-        
+        Parameters
+        ----------
+        self : LintSampler
+            The LintSampler instance.
+
+        Returns
+        -------
+        combinations: numpy array
+            All possible combinations of starting and ending points for the
+            inferred dimensionality of the problem.
+                
         """
-        self._setgrid(grid)
+        self._setgrid(cells)
 
 
     def _setgrid(self,cells):
         """Construct the cells for sampling.
 
+        Parameters
+        ----------
+        self : LintSampler
+            The LintSampler instance.
+        cells : tuple of arrays or list of tuples of arrays
+            If a tuple of arrays, the boundary values for a grid with dimensionality of the length
+            of the tuple. If a list of tuples of arrays, the boundary values for an arbitrary
+            number of grids with dimensionality the length of the tuples.
 
-        eval_type
-        dim
-        edgearrays
-        edgedims
-
-        Cases we handle:
-            1. cells = (edges,edges)
-            2. cells = [(edges,edges),(different_edges,different_edges)]
+        Returns
+        -------
+        None
         
+        Attributes
+        -------
+        eval_type : string
+            The evaluation type, either 'gridsample' or 'freesample'
+        dim : int
+            The inferred dimensionality of the problem
+        edgearrays : list of numpy arrays
+            The arrays defining the edge of the grids
+        edgedims   : list of integers, optional, used if eval_type == 'gridsample'
+            The shape of the gridsample grid
+        ngrids : integer, optional, used if eval_type == 'freesample'
+            The number of distinct grids to be constructed
+        gridshape : list of tuple of integers, optional, used if eval_type == 'freesample'
+            The shape of each distinct grid
+        ngridentries : list of integers, optional, used if eval_type == 'freesample'
+            The number of entries in the 
+        nedgegridentries : list of integers, optional, used if eval_type == 'freesample'
+            The number of entries in the offset grids
+        x0 : list of numpy arrays, optional, used if eval_type == 'freesample'
+            The array of the first vertex of the grids
+        x1 : list of numpy arrays, optional, used if eval_type == 'freesample'
+            The array of the last vertex of the grids
+
         
         """
+
+        # check the validity of the input
+        if len(cells)==0:
+            raise ValueError("LintSampler._setgrid: you must specify an evaluation domain with a tuple of arrays or a list of tuples of arrays. See documentation for details.")
+            # or, we could here drop into an adaptive grid selection
+            # or, we could sample on a unit hypercube with dimensions of the pdf
+
         # 0. are we in the 1d case? the input is just a single tuple or 1d array.
-        # i.e. cells = (-12,12,100) or cells = np.linspace(-12,12,100)
-        if (isinstance(cells[0],float) | (isinstance(cells[0],int))):
+        # i.e. cells = np.linspace(-12,12,100)
+        if (isinstance(cells[0],float) | (isinstance(cells[0],int))): # do we need the integer option here? I think no?
             self.eval_type='gridsample'
 
             # override the inferred dimensionality
             self.dim = 1
 
             # set the arrays
-            if len(cells)==3: # we are being passed a (min,max,n) tuple
-                self.edgearrays = np.linspace(cells[0],cells[1],cells[2])
-                self.edgedims = cells[2]
-            else: # we are being passed the array directly
-                self.edgearrays = cells
-                self.edgedims = len(cells)
+            # we are being passed the array directly
+            self.edgearrays = cells
+            self.edgedims = len(cells)
 
 
         # 1. tuples defining the array -> make arrays, pass to gridsample
-        # i.e. cells = ((-12,12,100),(-4,4,50))
-        if isinstance(cells[0],tuple):
+        # i.e. cells = (np.linspace(-12,12,100),np.linspace(-4,4,50))
+        if isinstance(cells,tuple):
+
+            if isinstance(cells[0],tuple):
+                raise ValueError("LintSampler: Cells must be a single tuple or a list of tuples (i.e. not a tuple of tuples).")
+            
+            # infer the dimensionality
+            self.dim = len(cells)
+
+            # set the evaluation type
             self.eval_type = 'gridsample'
 
             # keep track of the edge arrays
@@ -276,83 +322,71 @@ class LintSampler:
 
             # create the grid
             for d in range(0,self.dim):
-                self.edgearrays.append(np.linspace(cells[d][0],cells[d][1],cells[d][2]))
-                self.edgedims[d] = cells[d][2]
+                self.edgearrays.append(cells[d])
+                self.edgedims[d] = len(cells[d])
 
+        # 2. a list of tuples defining multiple arrays
+        # i.e. cells = [(np.linspace(-12,0,100),np.linspace(-4,0,50)),(np.linspace(0,12,100),np.linspace(0,4,50))]
+        if isinstance(cells,list):
+            self.eval_type = 'freesample'
 
-        # two switches for an array input
-        if isinstance(cells[0],np.ndarray):
+            # how many grid boundaries are we being handed?
+            self.ngrids = len(cells)
 
-            # 2. the grid values themselves -> gridsample. check the .dim numpy attribute
-            if cells[0].ndim == 1:
-                self.eval_type = 'gridsample'
+            # infer the dimensionality: this will work whether the input is a single grid or list of grids
+            self.dim = len(cells[0])
 
-                # keep track of the edge arrays
-                self.edgearrays = []
+            # check special case dimensionality
+            if self.dim==1:
+                raise NotImplementedError("LintSampler: For 1d problems, the grid must be continuous.")
 
-                # keep track of the dimensions
-                self.edgedims = np.ones(self.dim,dtype='int')
+            # create holding arrays
+            grids                 = [] # the grids constructed from the inputs (no need to expose)
+            self.gridshape        = [] # the shape of the input grids
+            self.ngridentries     = [] # the number of entries in the grid
+            self.nedgegridentries = [] # the number of entries in the edge grid
+            self.edgearrays       = [] # the flattened grids, each [ngridentries,dim]
 
-                # store the grid
-                for d in range(0,self.dim):
-                    self.edgearrays.append(cells[d])
-                    self.edgedims[d] = len(cells[d])
+            # loop through the boundaries, construct the grids, and flatten
+            for ngrid in range(0,self.ngrids):
 
+                # check that the dimensions are properly specified for the grid
+                if len(cells[ngrid])!=self.dim:
+                    raise ValueError("LintSampler._setgrid: All input cells must have the same dimensionality.")
 
-            # 3. the grid points themselves; each entry in grid is a separate grid 
-            else:
-                self.eval_type = 'freesample'
+                # construct the grid
+                grid = np.stack(np.meshgrid(*cells[ngrid], indexing='ij'), axis=-1)
+
+                # save the grid
+                grids.append(grid)
+
+                # construct the grid boundary helpers
+                self.gridshape.append(grid.shape)
+                self.ngridentries.append(np.prod(grid.shape[0:self.dim]))
+                self.nedgegridentries.append(np.prod(np.array(grid.shape[0:self.dim])-1))
+
+                # flatten the grid into a [...,dim] array for passing to .pdf
+                self.edgearrays.append(grid.reshape(self.ngridentries[ngrid],self.dim))
             
-                # the grids must be [...,dim]
-
-
-                # infer the dimensionality: this will work whether the input is a single grid or list of grids
-                self.dim = cells[0].shape[-1]
-
-                if self.dim==1:
-                    # really, we shouldn't ever be in this case, because it's just a grid.
-                    # now we are looking at the edges of the grids, 
-                    self.x0 = cells[0] # the lower boundaries for the cells
-                    self.x1 = cells[1] # the upper boundaries for the cells
-
-                else:
-
-                    # check: is this a list of grids?
-                    if (isinstance(cells,list)|(isinstance(cells,tuple))):
-                        # how many grids are we being handed?
-                        self.ngrids = len(cells)
-
-                        # loop through the grids and flatten
-                        self.gridshape = []
-                        self.ngridentries = [] # the number of entries in the grid
-                        self.nedgegridentries = [] # the number of entries in the edge grid
-                        self.edgearrays = [] # the flattened grids, each [ngridentries,dim]
-                        for ngrid in range(0,self.ngrids):
-                            self.gridshape.append(cells[ngrid].shape)
-                            self.ngridentries.append(np.prod(cells[ngrid].shape[0:self.dim]))
-                            self.nedgegridentries.append(np.prod(np.array(cells[ngrid].shape[0:self.dim])-1))
-                            self.edgearrays.append(cells[ngrid].reshape(self.ngridentries[ngrid],self.dim))
-                            # now self.edgearrays is a list of flattened grids, and can be passed to self.pdf
-                        
-                        self.x0 = np.vstack([cells[ngrid][tuple([slice(0,self.gridshape[ngrid][griddim]-1) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries[ngrid],self.dim)) for ngrid in range(0,self.ngrids)])
-                        self.x1 = np.vstack([cells[ngrid][tuple([slice(1,self.gridshape[ngrid][griddim]  ) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries[ngrid],self.dim)) for ngrid in range(0,self.ngrids)])
-
-                    
-                    else:
-                        # not a list of grids, so this must be a single grid. special case handling...
-                        self.ngrids = 0
-
-                        # special case of a single grid
-                        self.gridshape = cells.shape
-                        self.ngridentries = np.prod(cells.shape[0:self.dim]) # the number of entries in the grid
-                        self.nedgegridentries = np.prod(np.array(cells.shape[0:self.dim])-1) # the number of entries in the grid
-                        self.edgearrays = cells.reshape(self.ngridentries,self.dim) # the flattened grid, [nengrids,dim]
-
-                        self.x0 = np.vstack([cells[tuple([slice(0,self.gridshape[griddim]-1) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries,self.dim))])
-                        self.x1 = np.vstack([cells[tuple([slice(1,self.gridshape[griddim]  ) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries,self.dim))])
+            self.x0 = np.vstack([grids[ngrid][tuple([slice(0,self.gridshape[ngrid][griddim]-1) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries[ngrid],self.dim)) for ngrid in range(0,self.ngrids)])
+            self.x1 = np.vstack([grids[ngrid][tuple([slice(1,self.gridshape[ngrid][griddim]  ) for griddim in range(0,self.dim)])].reshape((self.nedgegridentries[ngrid],self.dim)) for ngrid in range(0,self.ngrids)])
 
 
     def _get_startend_points(self):
+        """Return all combinations of start and end points for offset grids.
+        
+        Parameters
+        ----------
+        self : LintSampler
+            The LintSampler instance.
+
+        Returns
+        -------
+        combinations: numpy array
+            All possible combinations of starting and ending points for the
+            inferred dimensionality of the problem.
+        
+        """
 
         # Create an array representing the start/end points
         arr = np.array([0, 1]) 
