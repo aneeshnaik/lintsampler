4c8e38dd285d8996871e0dae98ccde645db4a3af
Aneesh Naik <48957123+aneeshnaik@users.noreply.github.com>
2024-05-22 11:51:08 +0100

    Test remove PackageNotFoundError from __init__

--- src/lintsampler/__init__.py
+++ src/lintsampler/__init__.py
@@ -1,9 +1,9 @@
 from .lintsampler import LintSampler
 from importlib.metadata import version, PackageNotFoundError
 
-try:
-    __version__ = version("lintsampler")
-except PackageNotFoundError:
-    __version__ = "unknown version"
+#try:
+__version__ = version("lintsampler")
+#except PackageNotFoundError:
+#    __version__ = "unknown version"
 
 __all__ = ["LintSampler"]
